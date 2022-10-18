5ac5c9d97ccc76daa617628f37bd67c0f5c08e5e
Arturo Gonzalez <arturo.gonzalez@rtasoftware.com.mx>
2022-10-18 11:18:36 -0500

    initial commit

---
+++ useTodo.js
@@ -0,0 +1,62 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useTodo = () => {
+    
+    const [todos, dispatchTodo] = useReducer(todoReducer, [], init);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
+    
+
+    const onAddTodo = (todo) => {
+        
+        const todoAction = {
+            type: '[TODO] add',
+            payload: todo,
+        };
+
+        dispatchTodo(todoAction);
+
+    };
+
+    const onRemoveTodo = (id) => {
+        
+        const todoAction = {
+            type: '[TODO] remove',
+            payload: id,
+        }
+
+        dispatchTodo(todoAction);
+
+    };
+
+    const onToggleTodo = (id) => {
+
+        const todoAction = {
+            type: '[TODO] toggle',
+            payload: id,
+        }
+        
+        dispatchTodo(todoAction);
+
+    };
+
+    const todosCount = todos.length;
+
+    const pendingTodosCount = todos.filter(todo => todo.done === false).length;
+    
+    return {
+        todos,
+        todosCount,
+        pendingTodosCount,
+        onAddTodo,
+        onRemoveTodo,
+        onToggleTodo,
+    };
+}
